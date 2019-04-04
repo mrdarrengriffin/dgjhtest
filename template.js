d9f99999b1e91d5c1d9e84c3055d06da55d1275b
Darren Griffin <darren.griffin@s3.local>
2019-04-04 14:14:48 +0100

    Changed some stuff

--- template.js
+++ template.js
@@ -1,15 +1,14 @@
 function Template() {
-
-
+    
     this.variables = {
         "app-name": "S3MT",
         "app-version": "2.0.0"
     }
-    
+
     this.renderVariables = function() {
         Object.keys(this.variables).forEach(variable => {
-            var variableValue = this.variables[variable]
-            $("*[data-value='" + variable + "']").text(variableValue)
+            var value = this.variables[variable]
+            $("*[data-value='" + variable + "']").text(value)
         })
     }
 }
