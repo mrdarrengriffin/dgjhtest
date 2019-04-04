fdecef198b4bfa196754c3738d201e78f5ae1df2
Darren Griffin <darren.griffin@live.co.uk>
2019-04-04 14:50:25 +0100

    Fixed issue in interval calling in init

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 function init() {
-  setInterval(tick(), 1000)
+  setInterval(tick, 1000)
 }
 
-<<<<<<< HEAD
 function tick(){
     var now = new Date()
     $("#hour").text(now.getHours())
     $("#minute").text(now.getMinutes())
     $("#second").text(now.getSeconds())
 }
-=======
-function tick() {
-
-}
->>>>>>> 56d8338ea4472752dca0d38732dd3dac8925495d
