faec313b784a9f1f52eb57ce6455fdbc46c02527
Darren Griffin <darren.griffin@live.co.uk>
2019-04-04 15:26:11 +0100

    Added zero padding

--- app.js
+++ app.js
@@ -12,24 +12,28 @@ function init() {
 function tick() {
     var now = new Date()
     // Date
-    $("#date #dayname").text(locale.clock.days[now.getDay()-1])
+    $("#date #dayname").text(locale.clock.days[now.getDay() - 1])
     $("#date #day").text(now.getDate())
     $("#date #day-ordinal").text(getOrdinal(now.getDate()))
-    $("#date #month").text(locale.clock.months[now.getMonth()-1])
+    $("#date #month").text(locale.clock.months[now.getMonth() - 1])
     $("#date #year").text(now.getFullYear())
 
     // Time
     $("#time #hour").text(now.getHours())
     $("#time #minute").text(now.getMinutes())
-    $("#time #second").text(now.getSeconds())
+    $("#time #second").text(padZero(now.getSeconds()))
+}
+
+function padZero(i) {
+    return (i < 10 ? "0"+i : i)
 }
 
 function getOrdinal(d) {
-    if (d > 3 && d < 21) return 'th'; 
+    if (d > 3 && d < 21) return 'th';
     switch (d % 10) {
-      case 1:  return "st";
-      case 2:  return "nd";
-      case 3:  return "rd";
-      default: return "th";
+        case 1: return "st";
+        case 2: return "nd";
+        case 3: return "rd";
+        default: return "th";
     }
-  }
+}
