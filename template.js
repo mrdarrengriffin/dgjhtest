02d472e54cf9125dd558a3be51c64fb6a156d1e7
james090500 <james@fasttortoise.co.uk>
2019-04-04 14:20:15 +0100

    Improved some formatting

--- template.js
+++ template.js
@@ -1,16 +1,18 @@
+/* global $ */
+
 function Template() {
 
-    this.variables = {
-        "app-name": "S3MT",
-        "app-version": "2.0.0"
-    }
+  this.variables = {
+    'app-name': 'S3MT',
+    'app-version': '2.0.0'
+  }
 
-    this.renderVariables = () => {
-        Object.keys(this.variables).forEach(variable => {
-            var variableValue = this.variables[variable]
-            $("*[data-value='" + variable + "']").text(variableValue)
-        })
-    }
+  this.renderVariables = () => {
+    Object.keys(this.variables).forEach(variable => {
+      var variableValue = this.variables[variable]
+      $("*[data-value='" + variable + "']").text(variableValue)
+    })
+  }
 
-    yeet = true;
+  let yeet = true
 }
