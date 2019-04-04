2e60ac99c34c82a274ea6840258aa5d9ce9f4c30
Darren Griffin <darren.griffin@live.co.uk>
2019-04-04 15:10:47 +0100

    Added date formatting

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-function init() {
-  setInterval(tick, 1000)
+var locale = {
+    clock: {
+        days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    }
 }
 
-function tick(){
-    var now = new Date()
-    $("#hour").text(now.getHours())
-    $("#minute").text(now.getMinutes())
-    $("#second").text(now.getSeconds())
+function init() {
+    setInterval(tick, 1000)
 }
+
+function tick() {
+    var now = new Date()
+    // Date
+    $("#date #dayname").text(locale.clock.days[now.getDay()-1])
+    $("#date #day").text(now.getDate())
+    $("#date #day-ordinal").text(getOrdinal(now.getDate()))
+    $("#date #month").text(locale.clock.months[now.getMonth()-1])
+    $("#date #year").text(now.getFullYear())
+
+    // Time
+    $("#time #hour").text(now.getHours())
+    $("#time #minute").text(now.getMinutes())
+    $("#time #second").text(now.getSeconds())
+}
+
+function getOrdinal(d) {
+    if (d > 3 && d < 21) return 'th'; 
+    switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+  }
