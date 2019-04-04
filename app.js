5866af1cc90a41e97cef91ddc5115d8c071fc03d
Darren Griffin <darren.griffin@live.co.uk>
2019-04-04 14:47:41 +0100

    Added hours, minutes and seconds rendering in tick()

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ function init(){
 }
 
 function tick(){
-    
+    var now = new Date()
+    $("#hour").text(now.getHours())
+    $("#minute").text(now.getMinutes())
+    $("#second").text(now.getSeconds())
 }
