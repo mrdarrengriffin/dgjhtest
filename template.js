76b6baebe29c0799c6d5ece880959961f32c0cc9
james090500 <james@fasttortoise.co.uk>
2019-04-04 14:13:01 +0100

    Add yet variable and some formatting

--- template.js
+++ template.js
@@ -1,15 +1,16 @@
 function Template() {
 
-
     this.variables = {
         "app-name": "S3MT",
         "app-version": "2.0.0"
     }
-    
-    this.renderVariables = function() {
+
+    this.renderVariables = () => {
         Object.keys(this.variables).forEach(variable => {
             var variableValue = this.variables[variable]
             $("*[data-value='" + variable + "']").text(variableValue)
         })
     }
+
+    yeet = true;
 }
